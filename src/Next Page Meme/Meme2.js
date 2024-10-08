810cd1f54cfb70b4d3beabcb95232cd67bbcbb47
Vaibhav Sharma <81508584+Vaibhav9526@users.noreply.github.com>
2024-10-08 23:30:24 +0530

    Update Meme2.js

--- src/Next Page Meme/Meme2.js
+++ src/Next Page Meme/Meme2.js
@@ -23,7 +23,49 @@ const Meme2 = (props) => {
 
             <div className="container" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
                 <div className="memeDisplay my-5">
+                <u><h2>ONE PUNCH MAN</h2></u>
+                <br/>
+                <h4 className='SmallSize'>
+         ⣠⣴⣶⡋⠉⠙⠒⢤⡀⠀⠀⠀⠀⠀⢠⠖⠉⠉⠙⠢⡄⠀
+ <h4 className='SmallSize'>
+⠀⠀⠀⠀⠀⠀⢀⣼⣟⡒⠒⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀⢠⠃⠀⠀⠀⠀⠀⠹⡄
+ <h4 className='SmallSize'>
+⠀⠀⠀⠀⠀⠀⣼⠷⠖⠀⠀⠀⠀⠀⠀⠀⠀⠘⡆⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⢷
+ <h4 className='SmallSize'>
+⠀⠀⠀⠀⠀⠀⣷⡒⠀⠀⢐⣒⣒⡒⠀⣐⣒⣒⣧⠀⠀⡇⠀⢠⢤⢠⡠⠀⠀⢸
+ <h4 className='SmallSize'>
+⠀⠀⠀⠀⠀⢰⣛⣟⣂⠀⠘⠤⠬⠃⠰⠑⠥⠊⣿⠀⢴⠃⠀⠘⠚⠘⠑⠐⠀⢸
+ <h4 className='SmallSize'>
+⠀⠀⠀⠀⠀⢸⣿⡿⠤⠀⠀⠀⠀⠀⢀⡆⠀⠀⣿⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⣸
+ <h4 className='SmallSize'>
+⠀⠀⠀⠀⠀⠈⠿⣯⡭⠀⠀⠀⠀⢀⣀⠀⠀⠀⡟⠀⠀⢸⠀⠀⠀⠀⠀⠀⢠⠏
+ <h4 className='SmallSize'>
+⠀⠀⠀⠀⠀⠀⠀⠈⢯⡥⠄⠀⠀⠀⠀⠀⠀⡼⠁⠀⠀⠀⠳⢄⣀⣀⣀⡴⠃⠀
+ <h4 className='SmallSize'>
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⡦⣄⣀⣀⣀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠈⠉⠀
+                 <h4 className='SmallSize'>⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⠛⠃⠀⠀⠀⢹⠳⡶⣤⡤⣄⠀
+     <h4 className='SmallSize'>⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣠⢴⣿⣿⣿⡟⡷⢄⣀⣀⣀⡼⠳⡹⣿⣷⠞⣳⠀
+     <h4 className='SmallSize'>⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢰⡯⠭⠹⡟⠿⠧⠷⣄⣀⣟⠛⣦⠔⠋⠛⠛⠋⠙⡆
+     <h4 className='SmallSize'>⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢸⣿⠭⠉⠀⢠⣤⠀⠀⠀⠘⡷⣵⢻⠀⠀⠀⠀⣼⠀⣇
+     <h4 className='SmallSize'>⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⡇⣿⠍⠁⠀⢸⣗⠂⠀⠀⠀⣧⣿⣼⠀⠀⠀⠀⣯⠀⢸⠀
+     <h4 className='SmallSize'>⠀⠀                
+                
+                
 
+
+
+
+
+
+
+
+
+                
                 <u><h2>LOL</h2></u>
                 <br/>               
                     <h4 className='SmallSize'>╭╭━━━╮╮┈┈┈┈┈┈┈┈┈</h4>
